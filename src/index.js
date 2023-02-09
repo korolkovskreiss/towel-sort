476192e44a37ca5376a517c5b3f851646dd7f3d1
korolkovskreiss <113632378+korolkovskreiss@users.noreply.github.com>
2023-02-09 17:50:56 +0200

    fix: change code for undefined matrix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-   let result =[]
- for ( let i = 0; i < matrix.length; i++) {
- 		matrix[i].forEach( val => result.push(val))
- }
-return result
+ let result =[]
+	if (matrix === undefined) {
+  	return result;
+  }
+  else {
+  	for ( let i = 0; i < matrix.length; i++) {
+ 			matrix[i].forEach( val => result.push(val))
+ 		}
+result.sort((a,b) => a - b)
+return result;}
 }
